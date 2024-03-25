a0573ae87ef8f227691a9a157a31ddd496bc34bb
vts1978 <vinicioussouza@gmail.com>
2024-03-25 01:50:42 -0300

    program main

---
+++ Program.java
@@ -0,0 +1,32 @@
+public class Program {
+    public static void main(String[] args){
+
+        //Exercicio01.Executar();
+        //Exercicio02.Executar();
+        //Exercicio03.Executar();
+        //Exercicio04.Executar();
+        //Exercicio05.Executar();
+        //Exercicio06.Executar();
+        //Exercicio07.Executar();
+        //Exercicio08.Executar();
+        //Exercicio09.Executar();
+        //Exercicio10.Executar();
+        //Exercicio11.Executar();
+        //Exercicio12.Executar();
+        //Exercicio13.Executar();
+        //Exercicio14.Executar();
+        //Exercicio15.Executar();
+        //Exercicio16.Executar();
+        //Exercicio17.Executar();
+        //Exercicio18.Executar();
+        //Exercicio19.Executar();
+        //Exercicio20.Executar();
+        //Exercicio21.Executar();
+        //Exercicio22.Executar();
+        //Exercicio23.Executar();
+        //Exercicio24.Executar();
+        //Exercicio25.Executar();
+        //Exercicio26.Executar();
+
+    }
+}
